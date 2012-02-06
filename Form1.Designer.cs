abde55eb4174818b2480472e7b9e9361a3220906
Sebastien Lachance <sebastien.lachance@transcontinental.ca>
2012-02-06 12:53:42 -0500

    Functionnal working copy

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSelectSource = new System.Windows.Forms.Button();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.btnSelectDestination = new System.Windows.Forms.Button();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(86, 93);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(73, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start conversion";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnSelectSource
+            // 
+            this.btnSelectSource.Location = new System.Drawing.Point(12, 21);
+            this.btnSelectSource.Name = "btnSelectSource";
+            this.btnSelectSource.Size = new System.Drawing.Size(147, 23);
+            this.btnSelectSource.TabIndex = 1;
+            this.btnSelectSource.Text = "Select source folder";
+            this.btnSelectSource.UseVisualStyleBackColor = true;
+            this.btnSelectSource.Click += new System.EventHandler(this.btnSelectSource_Click);
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(165, 26);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(0, 13);
+            this.lblSource.TabIndex = 2;
+            this.lblSource.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnSelectDestination
+            // 
+            this.btnSelectDestination.Location = new System.Drawing.Point(12, 50);
+            this.btnSelectDestination.Name = "btnSelectDestination";
+            this.btnSelectDestination.Size = new System.Drawing.Size(147, 23);
+            this.btnSelectDestination.TabIndex = 3;
+            this.btnSelectDestination.Text = "Select a destination folder";
+            this.btnSelectDestination.UseVisualStyleBackColor = true;
+            this.btnSelectDestination.Click += new System.EventHandler(this.btnSelectDestination_Click);
+            // 
+            // lblDestination
+            // 
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.Location = new System.Drawing.Point(165, 55);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(0, 13);
+            this.lblDestination.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(681, 141);
+            this.Controls.Add(this.lblDestination);
+            this.Controls.Add(this.btnSelectDestination);
+            this.Controls.Add(this.lblSource);
+            this.Controls.Add(this.btnSelectSource);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Converting BlogEngine posts to Octopress";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnSelectSource;
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Button btnSelectDestination;
+        private System.Windows.Forms.Label lblDestination;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace BlogEngineToOctopress
 {
@@ -14,6 +16,66 @@ namespace BlogEngineToOctopress
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public string SourcePath { get; set; }
+        public string DestinationPath { get; set; }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Create the routes redirection
+            var redirection = new StringBuilder();
+
+            var files = Directory.GetFiles(SourcePath, "*.xml");
+            foreach (var file in files)
+            {
+                var content = XElement.Load(file);
+
+
+                var date = Convert.ToDateTime(content.Element("pubDate").Value);
+                var convertedDate = String.Format("{0}-{1}-{2}", date.Year, date.Month.ToString().PadLeft(2, '0'), date.Day.ToString().PadLeft(2, '0'));
+                var slug = content.Elements("slug").First().Value;
+                var filename = String.Format("{0}-{1}.markdown", convertedDate,
+                                             slug.ToLower().Replace("-", "-"));
+
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendLine("---");
+                sb.AppendLine("layout: post");
+                sb.AppendLine("title: \"" + content.Element("title").Value + "\"");
+                sb.AppendLine("date: " + content.Element("pubDate").Value);
+                sb.AppendLine("comments: true");
+                sb.AppendLine("categories: ");
+                sb.AppendLine("---");
+                sb.AppendLine("");
+                sb.Append(content.Element("content").Value);
+
+                redirection.Append("    r301 %r{^/post/" + slug + ".aspx?$}, '/blog/" + slug.ToLower() + "'" + Environment.NewLine);
+
+                File.WriteAllText(Path.Combine(DestinationPath ,filename), sb.ToString(), new UTF8Encoding(false));
+            }
+
+            File.WriteAllText( Path.Combine(DestinationPath, "redirection.rb"), redirection.ToString(), Encoding.UTF8);
+            MessageBox.Show("Done");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSelectSource_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            lblSource.Text = folderBrowserDialog1.SelectedPath;
+            SourcePath = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btnSelectDestination_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            lblDestination.Text = folderBrowserDialog1.SelectedPath;
+            DestinationPath = folderBrowserDialog1.SelectedPath;
         }
     }
 }
